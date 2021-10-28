716f76d48d8d0c0fd7e0e24f7b53f3ad7a2920a2
Cosmin29 <73159321+Cosmin29@users.noreply.github.com>
2021-10-28 13:05:23 +0300

    new_cimmit

--- Seminar4/Seminar4.js
+++ Seminar4/Seminar4.js
@@ -3,19 +3,16 @@ var signupButton = document.querySelector("form .signup-link a");
 var cancelLink = document.querySelector("form .signup-link a");
 
 signupLink = (() => {
-    console.log('Congratulations! You have successfully created an account');
-    signupButton = (numbers) => {
-        var total = 0;
-        for (var index = 1; index <= numbers; ++index) {
-            total += index;
-        }
-        return total;
-    }
-    console.log(`Sum of all numbers up to 10: ${signupButton(10)}`);
+    fetch('https://api.github.com/users/manishmshiva')
+    .then(response => response.json())  
+    .then(json => console.log(json)) 
+    .catch(err => console.log('Success !', err)); 
 });
 
 
-cancelLink = (()=>{
-    console.log('Warning! You are about to reset your inputs. Are you sure you want tocontinue ?');
-
+cancelLink = (()=> {
+   const div = document.getElementById('err');
+    const p = document.createElement('p');
+    p.innerText = 'Warning! You are about to reset your inputs. Are you sure you want to continue ?';
+    div.insertBefore(p, div.children[1]);
 });
