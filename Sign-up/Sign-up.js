345cf434a8f87ac70380a1f2ccb96a4e32559ded
Cosmin29 <73159321+Cosmin29@users.noreply.github.com>
2021-12-07 19:06:18 +0200

    s

--- Sign-up/Sign-up.js
+++ Sign-up/Sign-up.js
@@ -1,26 +1,6 @@
 var signupLink = document.querySelector('form .signup-link a');
 var signupButton = document.querySelector('form .signup-link a');
 var cancelLink = document.querySelector('form .signup-link a');
-const loginText = document.querySelector('.title-text .login');
-const loginForm = document.querySelector('form.login');
-const loginBtn = document.querySelector('label.login');
-const signupBtn = document.querySelector('label.signup');
-const signupLink = document.querySelector('form .signup-link a');
-
-signupBtn.onclick = () => {
-	loginForm.style.marginLeft = '-50%';
-	loginText.style.marginLeft = '-50%';
-};
-
-loginBtn.onclick = () => {
-	loginForm.style.marginLeft = '0%';
-	loginText.style.marginLeft = '0%';
-};
-
-signupLink.onclick = () => {
-	signupBtn.click();
-	return false;
-};
 
 signupLink = () => {
 	fetch('http://localhost:3005/users')
