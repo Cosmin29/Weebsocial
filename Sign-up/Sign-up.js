4a3963366bf0a0f40e8cbbc97b0302d5e7a4d027
Cosmin29 <73159321+Cosmin29@users.noreply.github.com>
2021-12-09 10:08:48 +0200

    New adds for login page

--- Sign-up/Sign-up.js
+++ Sign-up/Sign-up.js
@@ -1,17 +1,29 @@
-var signupLink = document.querySelector('form .signup-link a');
-var signupButton = document.querySelector('form .signup-link a');
-var cancelLink = document.querySelector('form .signup-link a');
+let signupBtnOnTheForm = document.querySelector('form .signup-link a');
+let signupButton = document.querySelector('form .signup-link a');
+let loginText = document.querySelector('.title-text .login');
+let loginForm = document.querySelector('form.login');
+let loginBtn = document.querySelector('label.login');
+let signupBtn = document.querySelector('label.signup');
+var signupLink = document.querySelector(' form.signup-link a');
+
+signupBtn = () => {
+	loginForm.style.marginLeft = '-50%';
+	loginText.style.marginLeft = '-50%';
+};
+
+loginBtn = () => {
+	loginForm.style.marginLeft = '0%';
+	loginText.style.marginLeft = '0%';
+};
 
 signupLink = () => {
+	signupBtn.click();
+	return false;
+};
+
+signupBtnOnTheForm = () => {
 	fetch('http://localhost:3005/users')
 		.then((response) => response.json())
 		.then((json) => console.log(json))
 		.catch((err) => console.log('Error', err));
-};
-
-cancelLink = () => {
-	const div = document.getElementById('err');
-	const p = document.createElement('p');
-	p.innerText = 'Warning! You are about to reset your inputs. Are you sure you want to continue ?';
-	div.insertBefore(p, div.children[1]);
 };
